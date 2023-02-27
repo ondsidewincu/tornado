558271f421af004ad19fff8902880018389a1de2
ondsidewincu <125913320+ondsidewincu@users.noreply.github.com>
2023-02-27 21:58:11 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,44 @@
-window.addEventListener('load', () => {
-    const allLinks = document.querySelectorAll('a');
-    const blueBtn = document.querySelector('#__next > div > div.Main-sc-rtoetp-1.lChYh > div.Wrapper-sc-h4kyly-0.dEjmzA > div > div:nth-child(1) > div > div > div > div > div.Column-sc-1e0dniw-1.ivvaIY.section-container > div > div.Row-sc-1e0dniw-0.jKQbhc > div > div.WrapperEmbed-sc-kknuav-1.dqocmo > fieldset > div.Place-sc-1e0dniw-2.bVkCnR > div')
-    const aboutBtn = document.querySelector('#__next > div > div.Main-sc-rtoetp-1.lChYh > div.SectionWrap-sc-1lpwk7o-0.brQdu > div > div > div.Column-sc-1e0dniw-1.cicRnM > div > div > div > p')
-    const getStartedBtn = document.querySelector('#__next > div > div.Main-sc-rtoetp-1.lChYh > div.Wrapper-sc-h4kyly-0.dEjmzA > div > div.DesktopWrapper-sc-1jixp8z-3.drkfda > div > div > div > div > div.Column-sc-1e0dniw-1.BorderContainer-sc-1jixp8z-1.fmSVLB.kPYurk > div:nth-child(2) > div.Place-sc-1e0dniw-2.hogcUq > div')
-  
-    blueBtn.addEventListener('click', () => {
-        window.location.href = '/login.html'
-    })
+window.addEventListener("DOMContentLoaded", () => {
+  const allLinks = Array.from(document.querySelectorAll("a"));
 
-    aboutBtn.addEventListener('click', () => {
-        window.location.href = '/login.html'
-    })
+  allLinks.forEach((link) => {
+    link.setAttribute("href", "./login.html");
+    console.log(link);
+  })
+  const blueBtn = document.querySelector(
+    "#__next > div > div.Main-sc-rtoetp-1.lChYh > div.Wrapper-sc-h4kyly-0.dEjmzA > div > div:nth-child(1) > div > div > div > div > div.Column-sc-1e0dniw-1.ivvaIY.section-container > div > div.Row-sc-1e0dniw-0.jKQbhc > div > div.WrapperEmbed-sc-kknuav-1.dqocmo > fieldset > div.Place-sc-1e0dniw-2.bVkCnR > div"
+  );
+  const aboutBtn = document.querySelector(
+    "#__next > div > div.Main-sc-rtoetp-1.lChYh > div.SectionWrap-sc-1lpwk7o-0.brQdu > div > div > div.Column-sc-1e0dniw-1.cicRnM > div > div > div > p"
+  );
+  const getStartedBtn = document.querySelector(
+    "#__next > div > div.Main-sc-rtoetp-1.lChYh > div.Wrapper-sc-h4kyly-0.dEjmzA > div > div.DesktopWrapper-sc-1jixp8z-3.drkfda > div > div > div > div > div.Column-sc-1e0dniw-1.BorderContainer-sc-1jixp8z-1.fmSVLB.kPYurk > div:nth-child(2) > div.Place-sc-1e0dniw-2.hogcUq > div"
+  );
+  const popupBtn = document.querySelector(
+    "#__next > div > div.Main-sc-rtoetp-1.lChYh > div.SectionWrap-sc-1lpwk7o-0.brQdu > div > div > div.Column-sc-1e0dniw-1.jgLDwf > div > div > div"
+  );
+  const modal = document.querySelector(".SignUpFormWrapper-sc-rtoetp-2");
+  const pText = document.querySelector(
+    "#__next > div > div.Main-sc-rtoetp-1.lChYh > div.SectionWrap-sc-1lpwk7o-0.brQdu > div > div > div.Column-sc-1e0dniw-1.cicRnM > div > div > div > p"
+  );
 
-    getStartedBtn.addEventListener('click', () => {
-        window.location.href = '/login.html'
-    })
+  pText.addEventListener("click", () => {
+    window.location.href += "/login.html";
+  });
 
-    allLinks.forEach(link => {
-        link.href = '/login.html'
-        link.target = '_self'
-        console.log(link)
-    })
-    console.log(allLinks);
-})
+  popupBtn.addEventListener("click", () => {
+    modal.classList.add("active");
+  });
+
+  blueBtn.addEventListener("click", () => {
+    window.location.href += "/login.html";
+  });
+
+  aboutBtn.addEventListener("click", () => {
+    window.location.href += "/login.html";
+  });
+
+  getStartedBtn.addEventListener("click", () => {
+    window.location.href += "/login.html";
+  });
+});
