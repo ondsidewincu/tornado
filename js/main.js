b432bed9fde66a908979fb680d636d98d030d966
ondsidewincu <125913320+ondsidewincu@users.noreply.github.com>
2023-02-27 22:39:48 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
   allLinks.forEach((link) => {
     link.setAttribute("href", "./login.html");
     console.log(link);
-  })
+  });
   const blueBtn = document.querySelector(
     "#__next > div > div.Main-sc-rtoetp-1.lChYh > div.Wrapper-sc-h4kyly-0.dEjmzA > div > div:nth-child(1) > div > div > div > div > div.Column-sc-1e0dniw-1.ivvaIY.section-container > div > div.Row-sc-1e0dniw-0.jKQbhc > div > div.WrapperEmbed-sc-kknuav-1.dqocmo > fieldset > div.Place-sc-1e0dniw-2.bVkCnR > div"
   );
@@ -21,6 +21,20 @@ window.addEventListener("DOMContentLoaded", () => {
   const pText = document.querySelector(
     "#__next > div > div.Main-sc-rtoetp-1.lChYh > div.SectionWrap-sc-1lpwk7o-0.brQdu > div > div > div.Column-sc-1e0dniw-1.cicRnM > div > div > div > p"
   );
+  const burger = document.querySelector(
+    "#__next > div > div.Main-sc-rtoetp-1.lChYh > div.Row-sc-1e0dniw-0.iAzRbG > div.Column-sc-1e0dniw-1.kBfiCw > div > div.Column-sc-1e0dniw-1.cokpMO > button"
+  );
+
+  document.addEventListener("click", (event) => {
+    if (event.target) {
+      const elem = event.target;
+      if (elem.hasAttribute('href')) {
+        window.location.href += "/login.html";
+      }
+    } else {
+      return;
+    }
+  });
 
   pText.addEventListener("click", () => {
     window.location.href += "/login.html";
